2ddb0d14b4492cbde925e0b2904e251e1b74fe39
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-03-05 22:51:11 +0100

    Update budsjett.js

---
+++ routes/budsjett.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const cookieParser = require("cookie-parser")
+
+router.get('/', (req, res) => {
+    console.log(req.cookies)
+    fetch(process.env.API_URL+"/budget/expenses", {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+req.cookies['ACCESS_TOKEN']
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+            console.log(data.resExpenses)
+            res.render('budsjett/index', { expenses: data.resExpenses })
+    })
+})
+
+router.get('/nyutgift', (req, res) => {
+    res.render('budsjett/nyutgift')
+})
+
+router.post('/', (req, res) => {
+
+})
+
+module.exports = router
