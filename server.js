0a5e1776c89bbc6d1dd9d1b9539ea9ea3eae6592
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-03-05 22:51:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+require('dotenv').config()
 const express = require('express')
+const cookie = require("cookie-parser")
+const budsjettRouter = require('./routes/budsjett')
+const loginRouter = require('./routes/login')
 const app = express()
 
 app.set('view engine','ejs')
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookie())
+
+app.use('/login', loginRouter)
+
+app.use('/budsjett', budsjettRouter)
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.send("Hello")
 })
 
 app.listen(5000)
