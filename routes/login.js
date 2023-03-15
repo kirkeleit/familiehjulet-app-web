9d0cb0192839d7469144a1d49a84d98fafa8b1c9
Thorbjørn Kirkeleit <thorbjorn@kirkeleit.net>
2023-03-15 21:55:11 +0100

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     console.log(req.body.EmailAddress)
     //res.send(req.body.EmailAddress)
-    fetch(process.env.API_URL+"/users/login/", {
+    fetch(process.env.BASE_API_URL+"/users/login/", {
         method: 'post',
         headers: {
           'Content-Type': 'application/json'
@@ -21,7 +21,7 @@ router.post('/', (req, res) => {
 
 router.get('/token/:token', (req, res) => {
     console.log(req.params.token)
-    fetch(process.env.API_URL+"/users/login/"+req.params.token, {
+    fetch(process.env.BASE_API_URL+"/users/login/"+req.params.token, {
         method: 'get',
         headers: {
           'Content-Type': 'application/json'
@@ -32,12 +32,29 @@ router.get('/token/:token', (req, res) => {
               console.log(data)
               res.cookie('ACCESS_TOKEN', data.AccessToken)
               res.cookie('REFRESH_TOKEN', data.RefreshToken)
-              res.render('login/successfull')
+              //res.render('login/successfull')
+              res.status(301).redirect("/")
       })
 })
 
 router.get('/register', (req, res) => {
     res.render('login/register')
+})
+
+router.post('/register', (req, res) => {
+    const User = {}
+    User.EmailAddress = req.body.EmailAddress
+    if (req.body.Forename) { User.Forename = req.body.Forename }
+    if (req.body.Surname) { User.Surname = req.body.Surname }
+    console.log(User)
+    fetch(process.env.BASE_API_URL+"/users/user", {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(User)
+      })
+      res.render('login/requested')
 })
 
 module.exports = router
